00a9f67415158b95538189fc5d2ed8bc66ed25bc
Oleksandr <pogrebnyak19999@gmail.com>
2023-08-01 20:20:09 +0200

    Refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 // const hamburger = document.querySelector('.hamburger'),
 //     menu = document.querySelector('.menu'),
 //     closeElem = document.querySelector('.menu__close');
-  
+
 // hamburger.addEventListener('click', () => {
 //     menu.classList.add('active');
 // });
@@ -14,3 +14,27 @@
 // percents.forEach( (item, i) => {
 //     rectangles[i].style.width = item.innerHTML;
 // });
+
+$("img.img-svg").each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr("class");
+  var imgURL = $img.attr("src");
+  $.get(
+    imgURL,
+    function (data) {
+      var $svg = $(data).find("svg");
+      if (typeof imgClass !== "undefined") {
+        $svg = $svg.attr("class", imgClass + " replaced-svg");
+      }
+      $svg = $svg.removeAttr("xmlns:a");
+      if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+        $svg.attr(
+          "viewBox",
+          "0 0 " + $svg.attr("height") + " " + $svg.attr("width")
+        );
+      }
+      $img.replaceWith($svg);
+    },
+    "xml"
+  );
+});
