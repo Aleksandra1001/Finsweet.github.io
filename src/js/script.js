d6d7ba864de0d574415e5d2b45d28015f7191f3e
Aleksandra1001 <allandra1029384756@gmail.com>
2023-09-24 19:45:54 +0200

    add script carousel

--- src/js/script.js
+++ src/js/script.js
@@ -41,20 +41,47 @@ $("img.img-svg").each(function () {
 
 
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        // infinite: false,
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/back_button.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/next_button.svg"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false
-              }
-            }
-        ]
-      });
+  $('.carousel__inner').slick({
+    infinite: false,
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-next"><img class="img-svg" src="../img/icons/back_button.svg"></button>',
+    nextArrow: '<button type="button" class="slick-prev"><img class="img-svg" src="../img/icons/back_button.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
   });
+
+  $("img.img-svg").each(function () {
+    var $img = $(this);
+    var imgClass = $img.attr("class");
+    var imgURL = $img.attr("src");
+    $.get(
+      imgURL,
+      function (data) {
+        var $svg = $(data).find("svg");
+        if (typeof imgClass !== "undefined") {
+          $svg = $svg.attr("class", imgClass + " replaced-svg");
+        }
+        $svg = $svg.removeAttr("xmlns:a");
+        if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+          $svg.attr(
+            "viewBox",
+            "0 0 " + $svg.attr("height") + " " + $svg.attr("width")
+          );
+        }
+        $img.replaceWith($svg);
+      },
+      "xml"
+    );
+  });
+});
+
+  
+
